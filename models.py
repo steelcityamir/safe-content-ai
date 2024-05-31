9712e389c4db6e80cf4a0b834b89465022d7af62
Boroumand, Amir A <amir@wireprivacy.org>
2024-05-31 16:57:21 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,49 @@
+# models.py
+
+from pydantic import BaseModel
+
+
+class ImageUrlsRequest(BaseModel):
+    """
+    Model representing the request body for the /api/v1/detect/urls endpoint.
+
+    Attributes:
+        urls (list[str]): List of image URLs to be processed.
+    """
+
+    urls: list[str]
+
+
+class ImageDetectionResponse(BaseModel):
+    """
+    Base model representing the response body for image detection.
+
+    Attributes:
+        is_nsfw (bool): Whether the image is classified as NSFW.
+        confidence_percentage (float): Confidence level of the NSFW classification.
+    """
+
+    is_nsfw: bool
+    confidence_percentage: float
+
+
+class FileImageDetectionResponse(ImageDetectionResponse):
+    """
+    Model extending ImageDetectionResponse with a file attribute.
+
+    Attributes:
+        file (str): The name of the file that was processed.
+    """
+
+    file_name: str
+
+
+class UrlImageDetectionResponse(ImageDetectionResponse):
+    """
+    Model extending ImageDetectionResponse with a URL attribute.
+
+    Attributes:
+        url (str): The URL of the image that was processed.
+    """
+
+    url: str
